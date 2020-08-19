b76a9876575469c110dd96e0096b0529ce377873
fjlcyl <50308326+fjlcyl@users.noreply.github.com>
2020-08-19 16:52:09 +0800

    Update Github.java

--- src/github/Github.java
+++ src/github/Github.java
@@ -5,5 +5,6 @@ public class Github {
 	public static void main(String[] args) {
 		System.out.println("fjl&cyl");
 		System.out.println("fjl Love cyl");
+		System.out.println("fjl Love cyl521");
 	}
 }
