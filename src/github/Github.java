338fd8b0905aca3ac071b8a531f7b992f428bd02
樊金良 <1925236293@qq.com>
2020-08-19 17:00:11 +0800

    dev1
    Signed-off-by: 樊金良 <1925236293@qq.com>

--- src/github/Github.java
+++ src/github/Github.java
@@ -7,5 +7,6 @@ public class Github {
 		System.out.println("fjl Love cyl");
 		System.out.println("fjl Love cyl521");
 		System.out.println("fjl Love cyl520");
+		System.out.println("fjl Love cyl520 Dev1");
 	}
 }
